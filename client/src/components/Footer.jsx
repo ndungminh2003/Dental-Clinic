18bfaf20d5844a66eee42b1ce13be6f7679689ae
lhnguyenVP21 <lhnguyen21@vp.fitus.edu.vn>
2023-11-10 10:17:00 +0700

    update profile

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import fb_pic from '../images/facebook.png'
+import ig_pic from '../images/instagram.png'
+import tw_pic from '../images/twitter.png'
+import clock from '../images/clock.png'
+
+
+export default function Footer() {
+    return (
+    <>
+    <div className="flex bg-yankees-blue justify-evenly items-center h-80 w-full">
+      <div className="container mx-auto grid grid-cols-3 gap-4 py-3 justify-items-center p-6">
+      <div className=" flex flex-col gap-7 ">
+        <div className="flex flex-row">
+            <div><img
+              src={fb_pic}
+              alt="Facebook"
+              className=" w-8 h-8 cursor-pointer "
+            />
+            </div>
+            <p className=" ml-2 flex items-center text-2xl text-grullo font-extrabold" >DentalCare</p>
+        </div>
+          <div className="flex flex-col gap-4">
+            <p className=" ml-2 text-grullo">Follow us on:</p>
+
+          <div className="flex gap-4 items-center ml-2">
+            <img
+              src={fb_pic}
+              alt="Facebook"
+              className=" w-8 h-8 cursor-pointer "
+            />
+            <img
+              src={ig_pic}
+              alt="Instagram"
+              className=" w-8 h-8 cursor-pointer "
+            />
+            <img
+              src={tw_pic}
+              alt="Twitter"
+              className=" w-8 h-8 cursor-pointer "
+            />
+            
+          </div>
+          </div>
+          
+          <div className="text-grullo ">
+                <h2>©DentalCare 2023. All Rights Reserved</h2>
+            </div>
+        </div>
+        <div className=" flex flex-col gap-5  text-grullo">
+          <h1 className="flex justify-center font-extrabold">QUICK LINKS</h1>
+          <p className=" cursor-pointer ml-2">About Us</p>
+          <p className=" cursor-pointer ml-2">Dental Services</p>
+          <p className=" cursor-pointer ml-2">Dentists</p>
+          <p className=" cursor-pointer ml-2">FAQs</p>
+        </div>
+
+        <div className=" flex flex-col gap-3 justify-center ">
+          <h1 className="font-extrabold text-grullo">CONTACT & INFORMATION</h1>
+          <div className="flex flex-row items-center gap-1">
+            <img src={fb_pic} alt="Facebook" className=" w-10 h-10"/>
+            <div className="flex flex-col ">
+                <p className="  ml-2 text-grullo">Phone number</p>
+                <p className="  ml-2 text-white text-xl">0989 123 456</p>
+            </div>
+          </div>
+          
+          <div className="flex flex-row items-center gap-1">
+            <img src={clock} alt="Facebook" className=" w-10 h-10  "/>
+            <div className="flex flex-col ">
+                <p className="  ml-2 text-grullo">Opening Hour</p>
+                <p className="  ml-2 text-white text-xl">07:30 AM - 17:10 PM</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row items-center gap-1">
+            <img src={fb_pic} alt="Facebook" className=" w-10 h-10 "/>
+            <div className="flex flex-col ">
+                <p className="  ml-2 text-grullo">Clinic Address</p>
+                <p className="  ml-2 text-white text-xl">227 Nguyen Van Cu, Ward 4, District 10, HCMC</p>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+    </div>
+    </>);
+}
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,19 +1,41 @@
 import React from "react";
+
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <>
-      <div className=" flex gap-10 bg-amber-500 justify-center">
-        <Link to="/" className=" text-2xl">
-          Home
-        </Link>
-        <Link to="/about" className=" text-2xl">
-          About
-        </Link>
-        <Link to="/login" className=" text-2xl">
-          Login
-        </Link>
+      <div className="flex w-full bg-blue-hosta justify-evenly items-center h-20">
+        <div className="logo flex justify-center items-center">LOGO</div>
+        <div className=" flex gap-24">
+          <Link to="/" className=" text-2xl hover:text-white">
+            Home
+          </Link>
+          <Link to="/about" className=" text-2xl hover:text-white">
+            About Us
+          </Link>
+          <Link to="/profile" className=" text-2xl hover:text-white">
+            Profile
+          </Link>
+          <Link to="/service" className=" text-2xl hover:text-white">
+            Services
+          </Link>
+          <br></br>
+          <Link to="/contact" className=" text-xl text-white font-bold bg-cyan-blue-azure rounded-lg w-60 h-10 flex justify-center items-center border-solid border-white hover:bg-ebony-clay">
+            Book an appointment
+          </Link>
+        </div> 
+        
+      <div className="flex gap-1">
+          <Link to="/login" className=" text-xl text-white bg-cyan-blue-azure hover:bg-ebony-clay">
+            Login
+          </Link>
+          <Link to="/signup" className=" text-xl text-white bg-cyan-blue-azure hover:bg-ebony-clay">
+            Signup
+          </Link>
+      </div>
+        
+        
       </div>
     </>
   );
